244da943a99f084bebc9beb132df183db5f30773
StanislavGeorgieff <stanislav.georgieff@gmail.com>
2015-08-18 22:03:32 +0300

    stash

---
+++ EasyHttp/src/EasyHttp/Contracts/IUriComposer.cs
@@ -0,0 +1,15 @@
+﻿namespace EasyHttp.Contracts
+{
+    public interface IUriComposer
+    {
+        /// <summary>
+        /// Method for composing URI.
+        /// </summary>
+        /// <param name="baseuri"> Base URI for the URI which we are composing.</param>
+        /// <param name="uri">URI for extending base URI.</param>
+        /// <param name="query">Query string.</param>
+        /// <param name="parametersAsSegments">Are parameters split as segments.</param>
+        /// <returns>Complete URI string.</returns>
+        string Compose(string baseuri, string uri, object query, bool parametersAsSegments);
+    }
+}
